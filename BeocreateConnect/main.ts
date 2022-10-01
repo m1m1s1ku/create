1c56d2a33b230068a5b72692cf9ef7c228f504b9
Leonard <leonard@mimisiku.dev>
2022-10-01 18:26:09 +0200

    fix copyrights

--- BeocreateConnect/main.ts
+++ BeocreateConnect/main.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2020 Bang & Olufsen A/S
+Copyright 2018-2022 Bang & Olufsen A/S
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
